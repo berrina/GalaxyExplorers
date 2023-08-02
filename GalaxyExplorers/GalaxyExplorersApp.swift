81bb80392477629f3f10ff780f64f6ce13af72da
Berrin Altinsoy <berrinaltinsoy3@gmail.com>
2023-08-02 14:55:46 -0400

    Initial Commit

---
+++ GalaxyExplorers/GalaxyExplorersApp.swift
@@ -0,0 +1,17 @@
+//
+//  GalaxyExplorersApp.swift
+//  GalaxyExplorers
+//
+//  Created by Berrin on 8/2/23.
+//
+
+import SwiftUI
+
+@main
+struct GalaxyExplorersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

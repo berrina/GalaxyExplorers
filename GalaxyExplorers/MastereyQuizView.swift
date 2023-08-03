747b59203c7dcfd17cc3efa4ff8edc55138d86ec
Berrin Altinsoy <berrinaltinsoy3@gmail.com>
2023-08-03 13:01:17 -0400

    i added mercury view and finished both home page and galaxy (solar system) page

---
+++ GalaxyExplorers/MastereyQuizView.swift
@@ -0,0 +1,20 @@
+//
+//  MastereyQuizView.swift
+//  GalaxyExplorers
+//
+//  Created by Berrin on 8/3/23.
+//
+
+import SwiftUI
+
+struct MastereyQuizView: View {
+    var body: some View {
+        Text("Create a mini quiz")
+    }
+}
+
+struct MastereyQuizView_Previews: PreviewProvider {
+    static var previews: some View {
+        MastereyQuizView()
+    }
+}
--- GalaxyExplorers/MercuryView.swift
+++ GalaxyExplorers/MercuryView.swift
@@ -4,13 +4,63 @@
 //
 //  Created by Berrin on 8/2/23.
 //
-
 import SwiftUI
+
 
 struct MercuryView: View {
     var body: some View {
-         Text ("blah blah for now")
-        
+        NavigationView {
+            ZStack {
+                Image("mercurywallpaper")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(1.0)
+                
+                VStack {
+                    Text("WANT TO CONQUER MERCURY?")
+                        .font(.largeTitle)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.black)
+                        .padding(.top, 30)
+                
+                    Image("mercuryimage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .padding(.top, 10)
+                    
+                    Spacer()
+                    
+                    NavigationLink(destination: GeneralFactsView()) {
+                        Text("General Facts")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.orange)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 50)
+                            .padding(.bottom, 10)
+                        Spacer ()
+                    }
+                    
+                    NavigationLink(destination: MastereyQuizView()) {
+                        Text("Mastery Quiz")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity)
+                            .padding()
+                            .background(Color.red)
+                            .cornerRadius(10)
+                            .padding(.horizontal, 50)
+                        Spacer ()
+                    }
+                }
+            }
+        }
     }
 }
 

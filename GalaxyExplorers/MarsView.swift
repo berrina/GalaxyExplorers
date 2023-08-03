02b203b2ee959d953f9d215781d043e8885ceeb7
Berrin Altinsoy <berrinaltinsoy3@gmail.com>
2023-08-03 14:52:01 -0400

    finished mars

--- GalaxyExplorers/MarsView.swift
+++ GalaxyExplorers/MarsView.swift
@@ -9,7 +9,91 @@ import SwiftUI
 
 struct MarsView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ZStack {
+                Image("marswallpaper")
+                    .resizable()
+                    .scaledToFill()
+                    .edgesIgnoringSafeArea(.all)
+                    .opacity(1.0)
+                
+                VStack {
+                    Text("LET'S EXPLORE MARS!")
+                        .font(.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.black)
+                        .padding(.top, 20)
+                
+                    Image("marsimage")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 150, height: 150)
+                        .padding(.top, 5.0)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("General Facts:")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .padding(.top, 5.0)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 20)
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                            
+                            VStack(alignment: .leading) {
+                                Text("It’s a cold desert, Bright red due to iron oxide dust.")
+                                    .padding(.vertical, 8)
+                                
+                                Text("Named after the Roman god of war.")
+                                    .padding(.vertical, 5)
+                                
+                                Text("Mars has two moons, Phobos and Deimos.")
+                                    .padding(.vertical, 20)
+                            }
+                            .padding()
+                        }
+                        .padding(.horizontal, 10.0)
+                        .padding(.bottom, 10.0)
+                        
+                        Text("Fun Facts:")
+                            .font(.title)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                        
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .foregroundColor(.white)
+                                .opacity(0.8)
+                            
+                            VStack(alignment: .leading) {
+                                Text("Beneath Mars' surface are sheets of frozen ice, and at both poles are ice caps.")
+                                    .padding(.vertical, 5)
+                                
+                                Text("Mars a long time ago was a much warmer, wetter world, rivers and maybe even oceans existed.")
+                                    .padding(.vertical, 5)
+                                
+                                Text("Has intense dust storms fueled by solar winds.")
+                                    .padding(.vertical, 5)
+                                
+                                Text("Mars shares the greatest similarities with Earth.")
+                                    .padding(.vertical, 5)
+                            }
+                            .padding()
+                        }
+                        .padding(.horizontal, 10.0)
+                        .padding(.bottom, 10.0)
+                        
+                        Spacer()
+                    }
+                    .padding(.top, 50)
+                    .foregroundColor(.black)
+                }
+            }
+        }
     }
 }
 
@@ -18,3 +102,4 @@ struct MarsView_Previews: PreviewProvider {
         MarsView()
     }
 }
+
